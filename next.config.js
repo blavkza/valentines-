e519a894348f79a40683e54eca716f9e7e5dfcaa
vhuhwavho <vvhuhwavho8@gmail.com>
2026-02-06 21:51:20 +0200

    first

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
